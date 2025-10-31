a2c0ef8f2896c544494000b5e010d02a0e34268e
Arjun Poudel <arjunpoudel479@gmail.com>
2025-10-31 10:31:48 +0545

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,66 +1,34 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { Container, Typography, Box, Stack, Link, Grid } from '@mui/material'
+import FacebookIcon from '@mui/icons-material/Facebook'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import MarkunreadIcon from '@mui/icons-material/Markunread'
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    <Container maxWidth="md" sx={{ py: 10, textAlign: 'center' }}>
+      <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" gap={4}>
+        <img src="/logo/logo-hr-removebg-preview.png" alt="Roila Logo" width={150} height={'auto'} />
+        <Typography variant="h5" >
+          Auto Parts Website Development & Integration Services
+        </Typography>
+        <Grid container spacing={4} justifyContent="center" mt={2}>
+          <Grid item>
+            <Link href="https://www.facebook.com/profile.php?id=61582460623161" target="_blank" rel="noopener">
+              <FacebookIcon sx={{ fontSize: 40, color: '#1877F2' }} />
+            </Link>
+          </Grid>
+          <Grid item>
+            <Link href="https://www.linkedin.com/company/109541409/" target="_blank" rel="noopener">
+              <LinkedInIcon sx={{ fontSize: 40, color: '#0077B5' }} />
+            </Link>
+          </Grid>
+          <Grid item>
+            <Link href="mailto:sales@roila.net">
+              <MarkunreadIcon sx={{ fontSize: 40, color: '#34A853' }} />
+            </Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
+  )
 }
